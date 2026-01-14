8f0cd7299a562ed357ae867991ab59c62543a7ca
Eka Setyawan <eka.setyawan0207@gmail.com>
2026-01-14 11:41:35 +0700

    activate api

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            LaguDaerahSeeder::class,
+        ]);
+
+
+
+
+
     }
 }
